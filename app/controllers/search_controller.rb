23e619621531816c82f4e0017c5dc7406324984c
Andrew Ledvina <wvvwwvw@gmail.com>
2016-04-22 01:10:55 -0700

    more search work

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,30 +1,34 @@
 class SearchController < ApplicationController
   before_action :require_internal_user
 
-  PERMITTED_KEYS = %i{ insured_id provider_id status from to result_types }
+  PERMITTED_KEYS = %w{ insured_id provider_id status from to result_types }
   RESULT_TYPES = %w{ submission claim }
+  SUBMISSION_QUERY_KEYS = %w{ insured_id provider_id }
+  CLAIM_QUERY_KEYS = %w{ insured_id provider_id status }
 
   def search
     query = search_params.select{|k,_| PERMITTED_KEYS.include? k}
-    if query[:result_types].present?
-      query[:result_types].select!{|k| RESULT_TYPES.include? k}
+    if query['result_types'].present?
+      query['result_types'].select!{|k| RESULT_TYPES.include? k}
     else
-      query[:result_types] = ['claim'] # claim is default search type
+      query['result_types'] = ['claim'] # claim is default search type
     end
 
     @submission_results, @claim_results = [], []
-    if query[:result_types].include?('submission')
-      @submisson_results = Submission.where(insured_id: query[:insured_id], provider_id: query[:provider_id])
+    if query['result_types'].include?('submission')
+      submission_query = query.select{|k,_| SUBMISSION_QUERY_KEYS.include? k}
+      @submission_results = Submission.where(submission_query)
     end
 
-    if query[:result_types].include?('claim')
-      @claim_results = Claim.where(insured_id: query[:insured_id], provider_id: query[:provider_id], status: query[:status])
-      if query[:from] && query[:to]
-        @claim_results = @claim_results.joins(:services).where(services: {date: query[:from]..query[:to]})
-      elsif query[:from]
-        @claim_results = @claim_results.joins(:services).where('services.date > ?', query[:from])
-      elsif query[:to]
-        @claim_results = @claim_results.joins(:services).where('services.date < ?', query[:to])
+    if query['result_types'].include?('claim')
+      claim_query = query.select{|k,_| CLAIM_QUERY_KEYS.include? k}
+      @claim_results = Claim.where(claim_query)
+      if query['from'] && query['to']
+        @claim_results = @claim_results.joins(:services).where(services: {date: query['from']..query['to']})
+      elsif query['from']
+        @claim_results = @claim_results.joins(:services).where('services.date > ?', query['from'])
+      elsif query['to']
+        @claim_results = @claim_results.joins(:services).where('services.date < ?', query['to'])
       end
     end
   end
