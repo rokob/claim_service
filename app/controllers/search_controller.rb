c9bdbeed26c98f3715f433c831c06834ac3bd660
Andrew Ledvina <wvvwwvw@gmail.com>
2016-04-21 23:56:36 -0700

    restrict search to internal users

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
+  before_action :require_internal_user
+
   def search
-    render json: {error: 'unimplemented'}
+    render json: {error: 'unimplemented'}, status: :not_implemented
   end
 end
