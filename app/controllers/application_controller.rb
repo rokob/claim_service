ec4ff9a8bf45f19356e378c9341fc8ac91f536b4
Andrew Ledvina <wvvwwvw@gmail.com>
2016-04-21 23:18:39 -0700

    add some auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  before_action: :authenticate
+
+  private
+    def authenticate
+      api_key = request.headers['X-Api-Key']
+      @user = User.where(api_key: api_key).first if api_key
+
+      unless @user
+        head status: :unauthorized
+        return false
+      end
+    end
+
+    def current_user
+      @user
+    end
 end
