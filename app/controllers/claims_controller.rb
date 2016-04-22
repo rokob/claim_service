7dc6d4d73f60aa3003ec3f4fa9e781e5ad1ded04
Andrew Ledvina <wvvwwvw@gmail.com>
2016-04-21 23:57:29 -0700

    restrict claims to internal users

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -1,9 +1,11 @@
 class ClaimsController < ApplicationController
+  before_action :require_internal_user
+
   def show
-    render json: {error: 'unimplemented'}
+    render json: {error: 'unimplemented'}, status: :not_implemented
   end
 
   def update
-    render json: {error: 'unimplemented'}
+    render json: {error: 'unimplemented'}, status: :not_implemented
   end
 end
